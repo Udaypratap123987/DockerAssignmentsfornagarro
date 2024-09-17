7bae4cd42ca99129d3b35b212303d422fc1d9e02
Uday Singh <uday.singh01@nagarro.com>
2024-09-17 10:31:38 +0530

    Initial commit of DockerAssignments1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://webapi"); // Use Docker service name
+
+            HttpResponseMessage response = await client.GetAsync("/weatherforecast");
+            string result = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine("Response from Web API:");
+            Console.WriteLine(result);
+        }
+    }
+}
